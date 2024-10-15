7cec09d923163e6337a0f4f7a05867db222c1f09
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-15 10:03:33 -0300

    create Navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+export function Navbar() {
+  return (
+    <nav className='h-[5vh] bg-secondary'>
+      <h1 className="text-3xl font-bold">Galeria Picsum</h1>
+
+      <ul>
+
+      </ul>
+    </nav>
+  )
+}
