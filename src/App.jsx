a53b8be74edaeab2498782acb7fd8860e6354147
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-15 10:11:41 -0300

    change App.jsx: refactor file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState, useEffect } from 'react';
-import { Card, Navbar } from './components';
-import api from './services/api';
+import { Navbar, Main } from './components';
 
 function App() {
-  const [galery, setGalery] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await api.get('/list');
-      setGalery(response.data);
-    })()
-  }, []);
-
   return (
     <div className="container w-full h-screen lg:max-w-7xl md:max-w-3xl mx:auto p-2">
       <Navbar />
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full h-full min-h-[90vh] lg:max-h-[90vh] overflow-y-auto no-scrollbar">
-        {galery.map(image => (
-          <Card key={image.id} image={image} />
-        ))}
-      </div>
+      <Main />
     </div>
 
   );
 }
 
 export default App;
-
-function delayForDemo(promise) {
-  return new Promise(resolve => {
-    setTimeout(resolve, 2000);
-  }).then(() => promise);
-}
