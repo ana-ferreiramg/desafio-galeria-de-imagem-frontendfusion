cc1bcec1ef979be17caf542bf764a3b269e5cae6
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-15 09:56:46 -0300

    get images with api (axios) and show on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-
-import './App.css'
+import { useState, useEffect } from 'react';
+import { Card, Navbar } from './components';
+import api from './services/api';
 
 function App() {
+  const [galery, setGalery] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('/list');
+      setGalery(response.data);
+    })()
+  }, []);
 
   return (
-    <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Desafio de Galeria de Imagens - Processo Seletivo Frontend Fusion</p>
-      </main>
-    </>
+    <div className="container w-full h-screen lg:max-w-7xl md:max-w-3xl mx:auto p-2">
+      <Navbar />
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full h-full min-h-[90vh] lg:max-h-[90vh] overflow-y-auto no-scrollbar">
+        {galery.map(image => (
+          <Card key={image.id} image={image} />
+        ))}
+      </div>
+    </div>
+
   );
 }
 
-export default App
+export default App;
+
+function delayForDemo(promise) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  }).then(() => promise);
+}
