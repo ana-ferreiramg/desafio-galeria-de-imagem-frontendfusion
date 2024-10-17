4a8a82768bbbc55f1e76f4e936ff2c5a319fa44a
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-17 16:45:55 -0300

    add flip function to card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,6 +5,7 @@ import heartSolid from '../../img/heart-solid.svg';
 
 export function Card({ image }) {
   const [isFavorite, setIsFavorite] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('galeryPicsum')) || [];
@@ -25,15 +26,35 @@ export function Card({ image }) {
     updateLocalStorage();
   }
 
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <div className="bg-transparent p-2 rounded-lg flex flex-col items-center max-w-80 max-h-[270px]">
-      <img src={image.download_url} alt={image.author} className="w-[330px] h-[210px]" loading='lazy' decoding="async" />
+      {isFlipped
+        ?
+        <>
+          <div className="w-full h-full text-center mt-2 font-semibold flex flex-col justify-between" onClick={handleFlip}>
+            <h2 className='text-xl font-bold underline underline-offset-2'>Informations</h2>
+            <p>{image.author}</p>
+            <div>
+              <p><a href={image.url} target='_blank'>Unsplash</a></p>
+              <p className='mt-2'><a href={image.url} target='_blank'>Picsum</a></p>
+            </div>
+          </div>
+        </>
+        :
+        <>
+          <img src={image.download_url} alt={image.author} className="w-[330px] h-[210px]" loading='lazy' decoding="async" onClick={handleFlip} />
 
-      <div className="text-center mt-2">
-        <button type="button" onClick={() => toggleFavorite(image)}>
-          {isFavorite ? <img src={heartSolid} alt="Favoritada" className='w-7' /> : <img src={heartRegular} alt="Não favoritada" className='w-7' />}
-        </button>
-      </div>
+          <div className="text-center mt-2">
+            <button type="button" onClick={() => toggleFavorite(image)}>
+              {isFavorite ? <img src={heartSolid} alt="Favoritada" className='w-7' /> : <img src={heartRegular} alt="Não favoritada" className='w-7' />}
+            </button>
+          </div>
+        </>
+      }
     </div>
   );
 }
