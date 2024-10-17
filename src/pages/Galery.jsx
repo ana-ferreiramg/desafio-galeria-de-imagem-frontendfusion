665c5ed93a1369b27b59af5edb7d7da13506767b
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-17 16:17:34 -0300

    add pagination page with useMemo and refactor code

--- src/pages/Galery.jsx
+++ src/pages/Galery.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect, useMemo } from 'react';
-
 import { Card } from '../components';
 import api from '../services/api';
 
+import left from '../img/left.svg';
+import right from '../img/right.svg';
+
 export function Galery() {
-  const [galery, setGalery] = useState([]);
+  const [galery, setGalery] = useState(JSON.parse(localStorage.getItem('galeryPicsum')) || []);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
+  const handlePageChange = (newPage) => (newPage > 0 && newPage <= Math.ceil(galery.length / itemsPerPage)) ? setCurrentPage(newPage) : setCurrentPage(currentPage);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,8 +21,6 @@ export function Galery() {
 
           setGalery(newData);
           localStorage.setItem('galeryPicsum', JSON.stringify(newData));
-        } else {
-          setGalery(JSON.parse(localStorage.getItem('galeryPicsum')));
         }
 
       } catch (error) {
@@ -27,11 +31,43 @@ export function Galery() {
     fetchData();
   }, []);
 
+  const currentGalery = useMemo(() => {
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    return galery.slice(indexOfFirstItem, indexOfLastItem);
+  }, [galery, currentPage, itemsPerPage]);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {galery ? galery.map(image => (
-        <Card key={image.id} image={image} />
-      )) : 'LOADING...'}
-    </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {currentGalery ? currentGalery.map(image => (
+          <Card key={image.id} image={image} />
+        )) : 'LOADING...'}
+      </div>
+
+      <div className="flex items-center justify-center mt-4">
+        {currentPage > 1 && (
+          <button
+            className="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded inline-flex items-center"
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            <img src={left} alt="Left" className='w-6' />
+            Previous
+          </button>
+        )}
+        <span className="mx-2 text-gray-700">
+          Page {currentPage} of {Math.ceil(galery.length / itemsPerPage)}
+        </span>
+        {currentPage < Math.ceil(galery.length / itemsPerPage) && (
+          <button
+            className="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded inline-flex items-center"
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            Next
+            <img src={right} alt="Left" className='w-6' />
+          </button>
+        )}
+      </div>
+    </>
   )
 }
