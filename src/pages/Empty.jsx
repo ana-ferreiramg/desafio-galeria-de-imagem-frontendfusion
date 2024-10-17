220b0eed9de18ecf744cf1f492391e7607237f9d
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-17 16:12:45 -0300

    fix style in Empty page

--- src/pages/Empty.jsx
+++ src/pages/Empty.jsx
@@ -2,9 +2,9 @@ import empty from '../img/empty.svg';
 
 export function Empty() {
   return (
-    <div className="relative w-full h-full flex flex-col items-center justify-center">
-      <img src={empty} alt="Page not found" className=' md:w-96 max-w-[550px] lg:w-2/5' />
-      <span className='absolute'>Não há nada aqui ainda :(</span>
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <img src={empty} alt="Page not found" className=' md:w-96 max-w-[550px]' />
+      <span>Não há nada aqui ainda :(</span>
     </div>
   )
 }
