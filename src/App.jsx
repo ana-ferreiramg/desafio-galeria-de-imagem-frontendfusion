3f5936936874f71438fea3a1c4a8ada73c608550
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-16 21:52:28 -0300

    change App.jsx: add routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { Navbar, Main } from './components';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import { Header } from './components';
+import { Galery, Favorites, NotFound } from './pages';
 
 function App() {
   return (
-    <div className="container w-full h-screen lg:max-w-7xl md:max-w-3xl mx:auto p-2">
-      <Navbar />
+    <Router>
+      <div className="w-full h-screen lg:max-w-7xl md:max-w-3xl relative max-h-screen mx:auto p-2">
+        <Header />
 
-      <Main />
-    </div>
+        <div className='w-full h-full min-h-[85vh] max-h-[85vh] md:max-h-[90vh] overflow-y-auto no-scrollbar'>
+          <Routes>
+            <Route path="/" exact element={<Galery />} />
+            <Route path="/galery" element={<Galery />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
 
   );
 }
